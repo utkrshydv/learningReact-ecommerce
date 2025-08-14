4ac6ca9c5040ce2e55b2d03ae7f97b1db37a0bc5
utkrshydv <utkarsh4socials@gmail.com>
2025-08-14 20:51:13 +0530

    complete orders page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router'
 import { HomePage } from './pages/HomePage'
 import { CheckoutPage } from './pages/CheckoutPage'
-
+import { OrdersPage } from './pages/OrdersPage'
 
 function App() {
 
@@ -9,7 +9,8 @@ function App() {
 
     <Routes>
       <Route index element={<HomePage />} />
-      <Route path="/checkout" element={<CheckoutPage />} />
+      <Route path="checkout" element={<CheckoutPage />} />
+      <Route path="orders" element={<OrdersPage/>}/>
     </Routes> 
   )
 }
